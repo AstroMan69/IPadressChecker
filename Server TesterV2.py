deeaa51be37bb6b6181daf10ad63ac6227eae9fa
AstroMan69 <95755638+AstroMan69@users.noreply.github.com>
2021-12-09 08:35:28 +0000

    Updated Version
    Fixed error in the code.

--- Server TesterV2.py
+++ Server TesterV2.py
@@ -9,17 +9,18 @@ import time
 #print("Your Computer Name is:" + hostname)    
 #print("Your Computer IP Address is:" + IPAddr) 
 
-ip_list = ['IP ADDRESS 1','IP ADDRESS 2', 'IP ADDRESS 3',]
+ip_list = ['IP Addresses']
 for ip in ip_list:
     response = os.popen(f"ping {ip}").read()
-    if "Received = 4" in response:
+    if "Destination host unreachable" not in response:
+    #if "Received = 4" in response:
         print(f"UP {ip} Ping Successful")
     else:
         i = 0
         x = 0
         while i < 3:
             response = os.popen(f"ping {ip}").read()
-            if "Received = 4" in response:
+            if "Destination host unreachable" not in response:
                 print(f"UP {ip} Ping Successful")
             else:
                 print(f"DOWN {ip} Ping Unsuccessful")
@@ -28,7 +29,7 @@ for ip in ip_list:
                 x = x + 1
                 while x < 3:
                     response = os.popen(f"ping {ip}").read()
-                    if "Received = 4" in response:
+                    if "Destination host unreachable" not in response:
                         print(f"UP {ip} Ping Successful")
                         i = 3
                         break
